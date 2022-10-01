d6b707d8819a673f62ffee92a724f0ed9f0e1e02
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2022-10-01 16:13:39 -0300

    criacao do projeto

---
+++ src/main/java/com/flexpag/paymentschedulermonitoring/PaymentSchedulerMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.flexpag.paymentschedulermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSchedulerMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSchedulerMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexpag/paymentschedulermonitoring/PaymentSchedulerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexpag.paymentschedulermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSchedulerMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
